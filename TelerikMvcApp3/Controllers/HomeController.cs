c82bd09b46a47880348876cfe130f46ee165b639
damiend16 <dconnaghan@fsai.ie>
2015-07-23 21:41:15 +0100

    1st check in

---
+++ TelerikMvcApp3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TelerikMvcApp3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Save(IEnumerable<HttpPostedFileBase> attachments, string id)
+        {
+            // The Name of the Upload component is "attachments"
+            foreach (var file in attachments)
+            {
+
+            }
+
+            // Return an empty string to signify success
+            return Content("");
+
+
+        }
+
+    }
+}
---
+++ TelerikMvcApp3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TelerikMvcApp3.MvcApplication" Language="C#" %>
